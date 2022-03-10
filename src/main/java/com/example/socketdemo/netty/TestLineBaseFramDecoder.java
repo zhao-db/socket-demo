fabf8b41c2305abe9f40ae367d97b7c4edb53cc2
dongbao.zhao <dongbao.zhao@geekplus.com>
2022-03-10 11:23:09 +0800

    行解码器

---
+++ src/main/java/com/example/socketdemo/netty/TestLineBaseFramDecoder.java
@@ -0,0 +1,34 @@
+package com.example.socketdemo.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author zhaodb 2022/3/10
+ * @since 3.0.1
+ */
+public class TestLineBaseFramDecoder {
+
+    public static void main(String[] args) {
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel(
+                new LineBasedFrameDecoder(1024),
+                new LoggingHandler(LogLevel.DEBUG)
+        );
+
+        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeBytes("Hello, world! \r\n".getBytes());
+        byteBuf.writeBytes("ni hao zbd ! \n".getBytes());
+        byteBuf.writeBytes("Hi! \r\n".getBytes());
+        embeddedChannel.writeInbound(byteBuf);
+
+    }
+
+}
